cc855b625fd4d746a15b800a9ecd27ecc51efe12
Jake Holtz <jahholtz@gmail.com>
2017-04-20 16:06:55 -0700

    Implemented basic requirements. Tests passing

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -1,23 +1,28 @@
 /*
- * Write a function WITH NO CALLBACKS that,
  * (1) reads a GitHub username from a `readFilePath`
  *     (the username will be the first line of the file)
  * (2) then, sends a request to the GitHub API for the user's profile
  * (3) then, writes the JSON response of the API to `writeFilePath`
- *
- * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
 var fs = require('fs');
 var Promise = require('bluebird');
 
 
+var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
+var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
+var writeFileAsync = Promise.promisify(fs.writeFile);
+
+// or Promise.promisifyAll(fs) then user fs.writeFileAsync
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then(getGitHubProfileAsync)
+    .then(function(profile) {
+      return writeFileAsync(writeFilePath, JSON.stringify(profile));
+    });
 };
 
-// Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
--- test/index.js
+++ test/index.js
@@ -4,10 +4,3 @@ describe('Bare Minimum Requirements', function() {
   require('./bare_minimum/promisification.test.js');
   require('./bare_minimum/basicChaining.test.js');
 });
-
-xdescribe('Advanced Content', function() {
- 
-  require('./advanced/collections.test.js');
-  require('./advanced/advancedChaining.test.js');
-  require('./advanced/PromiseLib.test.js');
-});
